aa2f1d80389deb375819b4cb399e1328f2e97b57
Neha Mahadik <neha.mahadik@ics-global.in>
2023-03-30 14:17:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import TopContainer from './components/TopContainer';
 import DashBoardStats from './components/DashBoardStats';
 import Header from './components/Header';
 import SlideOverlay from './components/SlideOverlay';
-import Income from "./components/Income";
-import Expense from "./components/Expense";
-import ViewExpenses from "./components/ViewExpenses";
-import Analytics from "./components/Analytics";
-import Transaction from "./components/Transaction";
-import Tranfermoney from "./components/Tranfermoney";
+// import Income from "./components/Income";
+// import Expense from "./components/Expense";
+// import ViewExpenses from "./components/ViewExpenses";
+// import Analytics from "./components/Analytics";
+// import Transaction from "./components/Transaction";
+// import Tranfermoney from "./components/Tranfermoney";
 function App() {
     return (
         <div className="w3-light-grey">
@@ -18,13 +18,13 @@ function App() {
             <DashBoardStats />
             <SlideOverlay />
             <Header />
-            <Income/>
+            {/* <Income/>
             <Expense/>
             <ViewExpenses/>
             <Analytics/>
             <Transaction/>
             <Tranfermoney/>
-      
+       */}
             
         </div>
     );
